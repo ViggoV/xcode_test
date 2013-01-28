e4502f0c38cfa822bc17d09652c16a53dc98a198
= <=>
2013-01-29 00:06:39 +0100

    Initial Commit

---
+++ IOS 6 Test1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOS 6 Test1
+//
+//  Created by Victor Eg Frølund on 29/01/135.
+//  Copyright (c) 2013 Victor Eg Frølund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOS 6 Test1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOS 6 Test1
+//
+//  Created by Victor Eg Frølund on 29/01/135.
+//  Copyright (c) 2013 Victor Eg Frølund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
